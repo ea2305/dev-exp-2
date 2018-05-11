e297d6c9b89dee155797b104ff8a567f18ab5d92
Elihu Cruz <ea230596@gmail.com>
2018-05-11 17:20:33 -0500

    basic dashboard card info system

--- pages/index.vue
+++ pages/index.vue
@@ -3,11 +3,16 @@
     <!-- Body -->
     <section class="container">
       <div class="columns is-multiline">
-        <div class="column is-3">
-          <card-info :title="'titulo'" :body="'aksjdf'" :image="'/icon.png'" :date="'2018-01-01'" :value="'1 project'"/>
+        <div class="column is-3" v-for="element of report" :key="element.title">
+          <card-info :title="element.title" 
+            :body="element.body" 
+            :image="element.image" 
+            :date="element.date" 
+            :value="element.value"/>
         </div>
       </div>
     </section>
+    <b-loading :is-full-page="true" :active.sync="isLoading" :canCancel="false"></b-loading>
   </div>
 </template>
 
@@ -16,12 +21,17 @@
 import CardInfo from '~/components/cards/info'
 
 export default {
+  data: () => ({
+    report: [],
+    isLoading: true
+  }),
   /**
    * Ignite component
    */
   beforeMount () {
     // define page attr
     this.setPage()
+    this.fetchAppInfo()
   },
   methods: {
     /**
@@ -30,6 +40,32 @@ export default {
     setPage () {
       this.$store.commit('setPageTitle', 'Dashboard')
       this.$store.commit('setPageDescription', 'Base route dashboard')
+    },
+    fetchAppInfo () {
+      // get data from firebase
+      let projectRef = this.$firebase.database().ref(`${process.env.APP_NAME}/project`);
+
+      let vm = this
+      projectRef.on('value', (snapshot) => {
+        // iteration
+        
+        let object = snapshot.val()
+        let projects = 0
+        // data to array projects
+        for (const key in object) 
+          if (object.hasOwnProperty(key)) 
+            projects++
+
+        this.report.push({
+          title: 'Projects',
+          body: 'Number of projects stored',
+          image: 'icon.png',
+          date: '',
+          value: `${projects} Projects`
+        })
+
+      })
+      this.isLoading = false
     }
   }, 
   components: {
