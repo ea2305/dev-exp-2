895ca8a39fa6d242499ecb102b85725a49b206d0
Elihu Cruz <ea230596@gmail.com>
2018-05-11 14:12:58 -0500

    Footer refactor style

--- components/layout/footer.vue
+++ components/layout/footer.vue
@@ -3,8 +3,11 @@
     <div class="container">
       <div class="content has-text-centered">
         <p>
-          <strong>dev^2</strong> by <a href="https://jgthms.com">Elihu A. Cruz</a>. The source code is licensed
+          
+          <strong>dev</strong><sup class="is-size-6">2</sup> by <a href="https://github.com/ea2305/dev-exp-2">Elihu A. Cruz</a>. The source code is licensed
           <a href="http://opensource.org/licenses/mit-license.php">MIT</a>.
+          <hr>
+          <img width="50" src="/icon.png" alt="dev2 icon">
         </p>
       </div>
     </div>
